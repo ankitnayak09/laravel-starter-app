3d16b8cb07a6b9a2ebe56f5afea370bb79b98290
Sourin <iamsourinsarkar@gmail.com>
2023-10-30 15:24:06 +0530

    Added CRUD features to the project

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css">
+</head>
+<body>
+ 
+    <div class="container">
+        <br>
+        @yield('content')
+    </div>
+
+</body>
+</html>
